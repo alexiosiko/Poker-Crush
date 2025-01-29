0ed89147b845ade58fd9383a9a7fa1e49a20cc4e
Alexi <jemimassyrup@gmail.com>
2025-01-28 16:16:32 -0800

    New asset pack

--- Assets/Scripts/Game/Card.cs
+++ Assets/Scripts/Game/Card.cs
@@ -15,18 +15,6 @@ public class Card : MonoBehaviour
 	public short number;
 	public Suit suit;
 	protected bool isBreaking = false;
- 	[SerializeField] Sprite face;
- 	[SerializeField] Sprite back;
-	public async Task Flip(int delayMilliseconds)
-	{
-		await Task.Delay(delayMilliseconds);
-		if (renderer.sprite == back)
-		{
-			await transform.DOScaleX(0, 0.1f).AsyncWaitForCompletion();
-			renderer.sprite = face;
-			await transform.DOScaleX(1, 0.1f).AsyncWaitForCompletion();
-		}
-	}
 	public virtual async Task Break()
     {
         if (isBreaking)
@@ -47,16 +35,6 @@ public class Card : MonoBehaviour
         // Destroy the object after animations
         Destroy(gameObject);
     }
-	new SpriteRenderer renderer;
-	void Start()
-	{
-		// _ = Flip(500);
-	}
-	void Awake()
-	{
-		renderer = GetComponent<SpriteRenderer>();
-		// renderer.sprite = back;
-	}
 }
 
 
